5a70210f76025381e0f67afa953e0ac050d44b5c
andreas <andreas@wellenvogel.de>
2019-04-14 22:30:09 +0200

    handle basic auth in internal browser, restart on reset

--- app/src/main/java/de/wellenvogel/bonjourbrowser/SettingsFragment.java
+++ app/src/main/java/de/wellenvogel/bonjourbrowser/SettingsFragment.java
@@ -1,17 +1,25 @@
 package de.wellenvogel.bonjourbrowser;
 
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.CookieManager;
 import android.webkit.WebStorage;
 import android.webkit.WebViewDatabase;
 import android.widget.TextView;
+
+import java.io.File;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +30,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public SettingsFragment() {
         // Required empty public constructor
     }
-
+    private void deleteFile(File f){
+        if (! f.exists()) return;
+        if (f.isDirectory()){
+            for (File cf:f.listFiles()){
+                deleteFile(cf);
+            }
+            f.delete();
+            return;
+        }
+        f.delete();
+    }
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences,rootKey);
@@ -30,9 +48,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                WebViewDatabase db=WebViewDatabase.getInstance(getContext());
-                db.clearHttpAuthUsernamePassword();;
-                WebStorage.getInstance().deleteAllData();
+                File base=getActivity().getCacheDir().getParentFile();
+                String deletes[]=new String[]{"app_webview","cache","databases"};
+                for (String ddir:deletes){
+                    deleteFile(new File(base,ddir));
+                }
+                System.exit(0);
                 return true;
             }
         });
--- app/src/main/java/de/wellenvogel/bonjourbrowser/WebViewActivity.java
+++ app/src/main/java/de/wellenvogel/bonjourbrowser/WebViewActivity.java
@@ -2,18 +2,28 @@ package de.wellenvogel.bonjourbrowser;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
+import android.text.InputType;
+import android.util.Log;
 import android.view.WindowManager;
+import android.webkit.HttpAuthHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebViewDatabase;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.lang.reflect.Method;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -26,8 +36,9 @@ public class WebViewActivity extends AppCompatActivity {
     private String serviceName;
     private URI serviceUri;
     private ProgressDialog pd;
-
     private class MyWebViewClient extends WebViewClient {
+        private String lastAuthHost="";
+        private String lastAuthRealm="";
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             return false;
@@ -39,6 +50,55 @@ public class WebViewActivity extends AppCompatActivity {
             super.onPageFinished(view, url);
             if (pd.isShowing()) pd.dismiss();
         }
+        @Override
+        public void onReceivedHttpAuthRequest(WebView view, final HttpAuthHandler handler, final String host, final String realm) {
+            String[] up = view.getHttpAuthUsernamePassword(host, realm);
+            if ((up != null && up.length == 2 ) &&  ! (lastAuthHost.equals(host) && lastAuthRealm.equals(realm))) {
+                handler.proceed(up[0], up[1]);
+            }
+            else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(WebViewActivity.this);
+                builder.setTitle(R.string.authentication);
+                builder.setMessage(host+" "+realm);
+                LinearLayout layout=new LinearLayout(WebViewActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+                layout.setPadding(20,0,20,0);
+                // Set up the inputs
+                final EditText username = new EditText(WebViewActivity.this);
+                final EditText password = new EditText(WebViewActivity.this);
+                username.setHint("Username");
+                password.setHint("Password");
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                username.setInputType(InputType.TYPE_CLASS_TEXT);
+                layout.addView(username);
+                layout.addView(password);
+                builder.setView(layout);
+                final WebView wv=view;
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        wv.setHttpAuthUsernamePassword(host,realm,username.getText().toString(),password.getText().toString());
+                        handler.proceed(username.getText().toString(),password.getText().toString());
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+                Log.e("", "Could not find username/password for domain: " + host + ", with realm = " + realm);
+            }
+            lastAuthRealm=realm;
+            lastAuthHost=host;
+        }
+
     }
 
     @Override
@@ -48,18 +108,18 @@ public class WebViewActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         webView=new WebView(this);
         setContentView(webView);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new MyWebViewClient());
-        webView.canZoomIn();
-        webView.canZoomOut();
-        webView.canGoBack();
         SharedPreferences sharedPref =
                 PreferenceManager.getDefaultSharedPreferences(this);
         Boolean keepOn=sharedPref.getBoolean(PREF_KEEP_ON,false);
         if (keepOn){
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.canZoomIn();
+        webView.canZoomOut();
+        webView.canGoBack();
         if (Build.VERSION.SDK_INT >= 16){
             try {
                 WebSettings settings = webView.getSettings();
