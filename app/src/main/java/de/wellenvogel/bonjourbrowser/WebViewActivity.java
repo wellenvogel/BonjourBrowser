307094fb7a4db1170bf394477b9bbea875c4f52b
andreas <andreas@wellenvogel.de>
2021-03-19 13:14:59 +0100

    correctly set screen brightness

--- app/src/main/java/de/wellenvogel/bonjourbrowser/WebViewActivity.java
+++ app/src/main/java/de/wellenvogel/bonjourbrowser/WebViewActivity.java
@@ -58,9 +58,15 @@ public class WebViewActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             int percent=msg.what;
-            float newBrightness=(float)percent/100;
-            if (newBrightness < 0.001f) newBrightness=0.001f;
-            if (newBrightness > 1) newBrightness=1;
+            float newBrightness;
+            if (percent >= 100){
+                newBrightness= WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE;
+            }
+            else {
+                newBrightness = (float) percent / 100;
+                if (newBrightness < 0.01f) newBrightness = 0.01f;
+                if (newBrightness > 1) newBrightness = 1;
+            }
             currentBrigthness=newBrightness;
             doSetBrightness(newBrightness);
         }
